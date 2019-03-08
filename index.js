22e4c95c434ba927f562ffecc5ce3ebc6b3bed6f
dsanderson90 <dsanderson3@cnm.edu>
2019-03-08 10:50:05 -0700

    added colors according to state density

--- index.js
+++ index.js
@@ -10,5 +10,28 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
     accessToken: accessToken
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+//L.geoJson(statesData).addTo(map);
 
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.density),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.geoJson(statesData, {style: style}).addTo(map);
