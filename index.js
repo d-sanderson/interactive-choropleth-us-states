9f8de3d842dc8b2f47ad0374189fc9220a9fc182
dsanderson90 <dsanderson3@cnm.edu>
2019-03-08 12:00:18 -0700

    added highlight (on mouseover), highlight reset (on mouseout), and zoom (onclick) event listeners  so the map is interactive

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
 //L.geoJson(statesData).addTo(map);
 
 function getColor(d) {
-    return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
+    return d > 1000 ? '#fff7fb' :
+           d > 500  ? '#ece7f2' :
+           d > 200  ? '#d0d1e6' :
+           d > 100  ? '#a6bddb' :
+           d > 50   ? '#74a9cf' :
+           d > 20   ? '#3690c0' :
+           d > 10   ? '#0570b0' :
+                      '#034e7b';
 }
 
 function style(feature) {
@@ -32,6 +32,47 @@ function style(feature) {
         dashArray: '3',
         fillOpacity: 0.7
     };
+
+   
 }
 
-L.geoJson(statesData, {style: style}).addTo(map);
+
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color:'#666',
+        dashArray: '',
+        fillOpacity: 0.7
+
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringtoFront();
+    }
+}
+
+let geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
